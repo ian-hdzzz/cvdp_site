98e9191933b965b95a05667875492710f0655c44
Ian <ianhdez2020@gmail.com>
2025-03-11 11:37:35 -0600

    actualilzación en el navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -55,7 +55,8 @@ function Navbar() {
                 {/* <li onClick={closeMenuOnMobile}>
                     <NavLink to="/maraton" className={({ isActive }) => (isActive ? "active" : "")}>Maratón de oportunidades laborales</NavLink>
                 </li> */}
-                <li onClick={closeMenuOnMobile}><NavLink href="https://queretarocvdp.my.canva.site/practicas" >Prácticas profesionales y empleo</NavLink></li>
+                <li onClick={closeMenuOnMobile}>
+                    <NavLink className={({ isActive }) => (isActive ? "active" : "")} href="https://queretarocvdp.my.canva.site/practicas" >Prácticas profesionales y empleo</NavLink></li>
                 <li onClick={closeMenuOnMobile}>
                     <NavLink to="/empresas" className={({ isActive }) => (isActive ? "active" : "")}>¿Eres empresa?</NavLink>
                 </li>
